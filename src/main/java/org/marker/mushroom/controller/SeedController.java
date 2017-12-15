3975c060ac9fb8dbbf000c99063559bc8c2d6d2d
keyusong <keyusong@hansap.com>
2017-12-15 18:02:07 +0800

    孵化项目管理入孵企业管理

---
+++ src/main/java/org/marker/mushroom/controller/SeedController.java
@@ -0,0 +1,88 @@
+package org.marker.mushroom.controller;
+
+import org.marker.mushroom.beans.Page;
+import org.marker.mushroom.beans.ResultMessage;
+import org.marker.mushroom.beans.Seed;
+import org.marker.mushroom.support.SupportController;
+import org.marker.mushroom.validator.ChipValidator;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+
+/**
+ * Created by 30 on 2017/12/15 0015.
+ * 孵化器项目管理
+ */
+@Controller
+@RequestMapping("/admin/seed")
+public class SeedController extends SupportController{
+
+
+	public SeedController() {
+		this.viewPath = "/admin/seed/";
+	}
+
+	//显示孵化项目页面
+	@RequestMapping("/list")
+	public ModelAndView list(Page page){
+		ModelAndView view = new ModelAndView(this.viewPath+"list");
+		view.addObject("page", commonDao.findByPage(page.getCurrentPageNo(),10,"select * from "+prefix+"seed order by id desc"));
+		return view;
+	}
+
+	//删除
+	@ResponseBody
+	@RequestMapping("/delete")
+	public Object delete(@RequestParam("rid") String rid){
+		Boolean struts = commonDao.deleteByIds(Seed.class, rid);
+		if(struts){
+			return new ResultMessage(true,"删除成功！");
+		}else{
+			return new ResultMessage(false,"删除失败！");
+		}
+	}
+
+	//添加功能页面展示
+	@RequestMapping("/add")
+	public ModelAndView add(){
+		ModelAndView view = new ModelAndView(this.viewPath+"add");
+		return view;
+	}
+
+	//保存操作
+	@ResponseBody
+	@RequestMapping("/save")
+	public Object save(@Validated(ChipValidator.class) Seed seed) {
+		seed.setTime(new Date());
+		if (commonDao.save(seed)) {
+			return new ResultMessage(true, "添加成功!");
+		} else {
+			return new ResultMessage(true, "添加失败!");
+		}
+	}
+
+	/**修改项目页面展示 */
+	@RequestMapping("/edit")
+	public ModelAndView edit(@RequestParam("id") long id) {
+		ModelAndView view = new ModelAndView(this.viewPath + "edit");
+		view.addObject("seed", commonDao.findById(Seed.class, id));
+		return view;
+	}
+
+	/** 修改操作 */
+	@ResponseBody
+	@RequestMapping("/update")
+	public Object update(Seed seed){
+		seed.setTime(new Date());
+		if (commonDao.update(seed)) {
+			return new ResultMessage(true, "更新成功!");
+		} else {
+			return new ResultMessage(true, "更新失败!");
+		}
+	}
+}
