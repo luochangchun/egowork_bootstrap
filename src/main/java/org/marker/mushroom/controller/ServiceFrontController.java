d4f0f94dcc5f9fac39a4029fc8effb33b580b691
30 <30@madoka.org>
2017-12-26 19:20:56 +0800

    服务产品接口

--- src/main/java/org/marker/mushroom/controller/ServiceFrontController.java
+++ src/main/java/org/marker/mushroom/controller/ServiceFrontController.java
@@ -4,11 +4,14 @@ import org.marker.mushroom.beans.ApplyService;
 import org.marker.mushroom.beans.ResultMessage;
 import org.marker.mushroom.support.SupportController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * 孵化器申请
@@ -35,4 +38,15 @@ public class ServiceFrontController extends SupportController {
 		}
 	}
 
+	/**
+	 * 获取服务产品
+	 * @param cid 产品分类id
+	 * @return
+	 */
+	@GetMapping("/{cid}")
+	@ResponseBody
+	public List get(@PathVariable("cid") int cid) {
+		String sql = "select * from mr_product where cid = " + cid;
+		return commonDao.queryForList(sql);
+	}
 }
