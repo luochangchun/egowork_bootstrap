be57c54295bea5ead15e3412b0e4a568ee9b69d5
zhangwei <123456>
2017-12-29 11:34:15 +0800

    提交活动列表页

--- src/main/webapp/themes/newego/js/banner.js
+++ src/main/webapp/themes/newego/js/banner.js
@@ -1,7 +1,7 @@
 var mySwiper = new Swiper('.swiper-container', {
     pagination: '.pagination',
     paginationClickable: true,
-    autoplay: 500000,
+    autoplay: 5000,
     speed: 1,
     // loop: true,
     //autoplayDisableOnInteraction : false,
